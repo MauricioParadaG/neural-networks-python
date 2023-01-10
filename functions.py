4a4f7ac975b61e6b4534c462a1da667ee938c367
Mauro <paradaj@uninorte.edu.co>
2023-01-10 17:54:18 +0100

    trainNeuronal: forward, backprop, Gradient descent

--- functions.py
+++ functions.py
@@ -2,63 +2,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # sigmoid function, continuous
+
 def sigmoid(x, derivate=False):
     if derivate:
         return np.exp(-x)/((1+np.exp(-x))**2)
     else:
         return 1/(1+np.exp(-x))
 
-x = np.linspace(-10, 10, 100)
-y = sigmoid(x)
-print(x) # [-10.  -9.79  -9.59   -9.39 
-plt.plot(x, y)
-plt.show()
-
-
-# step function, discrete
-def step(x):
-    return np.piecewise(x, [x < 0.0, x >= 0.0], [0, 1])
-
-x = np.linspace(-10, 10, 100)
-y = step(x)
-print(x)
-plt.plot(x, y)
-plt.show()
-
-
-# ReLU function, continuous
 def relu(x, derivate=False):
     if derivate:
         x[x<=0] = 0
         x[x>0] = 1
         return x
     else:
-        return np.piecewise(x, [x < 0.0, x >= 0.0], [0, lambda x: x])
-
-x = np.linspace(-10, 10, 100)
-y = relu(x)
-plt.plot(x, y)
-plt.show()
-
-# Tanh function, continuous
-def tanh(x):
-    return np.tanh(x)
-
-x = np.linspace(-10, 10, 100)
-y = tanh(x)
-plt.plot(x, y)
-plt.show()
-
+        return np.maximum(0, x)
 
 # mse mean squared error, loss function
 def mse(y, y_hat, derivate=False):
     if derivate:
-        return 2 * (y_hat - y) / y.size
+        return 2 * (y_hat - y)
     return np.mean((y - y_hat)**2)
 
-real = np.array([1, 2, 3, 4, 5])
-prediction = np.array([1, 2, 3, 4, 3])
-print(mse(real, prediction)) # 0.8
+
+def run ():
+    def sigmoid(x, derivate=False):
+        if derivate:
+            return np.exp(-x)/((1+np.exp(-x))**2)
+        else:
+            return 1/(1+np.exp(-x))
+
+    x = np.linspace(-10, 10, 100)
+    y = sigmoid(x)
+    print(x) # [-10.  -9.79  -9.59   -9.39 
+    plt.plot(x, y)
+    plt.show()
+
+
+    # step function, discrete
+    def step(x):
+        return np.piecewise(x, [x < 0.0, x >= 0.0], [0, 1])
+
+    x = np.linspace(-10, 10, 100)
+    y = step(x)
+    print(x)
+    plt.plot(x, y)
+    plt.show()
+
+
+    # ReLU function, continuous
+    def relu(x, derivate=False):
+        if derivate:
+            x[x<=0] = 0
+            x[x>0] = 1
+            return x
+        else:
+            return np.piecewise(x, [x < 0.0, x >= 0.0], [0, lambda x: x])
+
+    x = np.linspace(-10, 10, 100)
+    y = relu(x)
+    plt.plot(x, y)
+    plt.show()
+
+    # Tanh function, continuous
+    def tanh(x):
+        return np.tanh(x)
+
+    x = np.linspace(-10, 10, 100)
+    y = tanh(x)
+    plt.plot(x, y)
+    plt.show()
+
+
+    # mse mean squared error, loss function
+    def mse(y, y_hat, derivate=False):
+        if derivate:
+            return 2 * (y_hat - y) / y.size
+        return np.mean((y - y_hat)**2)
+
+    real = np.array([1, 2, 3, 4, 5])
+    prediction = np.array([1, 2, 3, 4, 3])
+    print(mse(real, prediction)) # 0.8
 
 
 
--- neuronalNumpy.py
+++ neuronalNumpy.py
@@ -1,29 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_gaussian_quantiles
+from functions import relu, sigmoid, mse
 
 n = 1000
 guassion_quantiles =  make_gaussian_quantiles(mean=None, cov=0.1, n_samples=n, n_features=2, n_classes=2, shuffle=True, random_state=None)
 
-x, y = guassion_quantiles # x is the data, y is the label
+x_data, y_label = guassion_quantiles
 
-print(x.shape) # (1000, 2)
-print(y.shape) # (1000,)
+print(x_data.shape) # (1000, 2)
+print(y_label.shape) # (1000,)
 
-y = y[: , np.newaxis] # (1000, 1)
+y_label = y_label[: , np.newaxis] # (1000, 1)
 
-plt.scatter(x[:, 0], x[:, 1], c=y[:, 0], s=40, cmap=plt.cm.Spectral)
-plt.show()
+plt.scatter(x_data[:, 0], x_data[:, 1], c=y_label[:, 0], s=40, cmap=plt.cm.Spectral)
+# plt.show()
 
 def initialize_parameters(layers_dims):
     parameters = {}
     L = len(layers_dims)
     for l in range(0, L-1):
         parameters['W' + str(l+1)] = (np.random.rand(layers_dims[l], layers_dims[l+1]) * 2) - 1 
-        parameters['b' + str(l+1)] = (np.zeros((1, layers_dims[l+1])) * 2) - 1
+        parameters['b' + str(l+1)] = (np.random.rand(1, layers_dims[l+1]) * 2) - 1
     return parameters
 
 layers_dims = [2, 4, 8, 1] # 2 inputs, 4 neurons, 8 neurons, 1 output
 parameters = initialize_parameters(layers_dims)
-print(parameters)
+# print(parameters)
+
+print(parameters['W1'].shape) # (2, 4)
+
+np.matmul(x_data, parameters['W1']) # (1000, 4)
+
+
+def train (x_data, learning_rate, parameters, training = True):
+# Forward propagation
+  parameters['A0'] = x_data
+
+  parameters['Z1'] = np.matmul(parameters['A0'], parameters['W1']) + parameters['b1']
+  parameters['A1'] = relu(parameters['Z1'])
+
+
+  parameters['Z2'] = np.matmul(parameters['A1'], parameters['W2']) + parameters['b2']
+  parameters['A2'] = relu(parameters['Z2'])
+
+  parameters['Z3'] = np.matmul(parameters['A2'], parameters['W3']) + parameters['b3']
+  parameters['A3'] = sigmoid(parameters['Z3'])
+
+  print(parameters['A3'].shape) # (1000, 1)
+
+  output = parameters['A3']
+  #print (output)
+
+  if training:
+    # Back propagation
+    parameters['dZ3'] = mse(y_label, output, True) * sigmoid(parameters['Z3'], True)
+    parameters['dW3'] = np.matmul(parameters['A2'].T, parameters['dZ3'])
+
+    parameters['dZ2'] = np.matmul(parameters['dZ3'], parameters['W3'].T) * relu(parameters['Z2'], True)
+    parameters['dW2'] = np.matmul(parameters['A1'].T, parameters['dZ2'])
+
+    parameters['dZ1'] = np.matmul(parameters['dZ2'], parameters['W2'].T) * relu(parameters['Z1'], True)
+    parameters['dW1'] = np.matmul(parameters['A0'].T, parameters['dZ1'])
+
+    # Gradient descent - update W and b
+    parameters['W3'] = parameters['W3'] - learning_rate * parameters['dW3']
+    parameters['b3'] = parameters['b3'] - learning_rate * parameters['dZ3']
+
+    parameters['W2'] = parameters['W2'] - learning_rate * parameters['dW2']
+    parameters['b2'] = parameters['b2'] - learning_rate * parameters['dZ2']
+
+    parameters['W1'] = parameters['W1'] - learning_rate * parameters['dW1']
+    parameters['b1'] = parameters['b1'] - learning_rate * parameters['dZ1']
+
+  return output
+
+output = train(x_data, 0.1, parameters = parameters, training = True)
+
+print (output)
+print (output.shape) # (1000, 1)
 
