0a341373b47f84869c95e42256ee251447523379
Mauro <paradaj@uninorte.edu.co>
2023-01-12 14:11:31 +0100

    3 layers model created and tested 88% - overfit

--- imdbBinaryClassification.py
+++ imdbBinaryClassification.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.datasets import imdb
+import matplotlib.pyplot as plt
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
@@ -28,5 +29,45 @@ x_test = vectorizeSequence(test_data)
 
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
+
+model = Sequential()
+model.add(Dense(16, activation='relu', input_shape=(10000,)))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(
+ partial_x_train, partial_y_train,
+ epochs=4,
+ batch_size=512,
+ validation_data=(x_val, y_val))
+
+history_dict = history.history
+#print(history_dict)
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+epochs = range(1, len(loss_values) + 1)
+
+plt.plot(epochs, loss_values, 'o', label='Training loss')
+plt.plot(epochs, val_loss_values, '--', label='Validation loss') 
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+results = model.evaluate(x_test, y_test)
+print(results) # [0.2896219491958618, 0.883400022983551]
+
+ 
 
 
